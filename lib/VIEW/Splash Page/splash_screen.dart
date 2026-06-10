cec99901ac2e35eac1020d31e17c327c57caa881
Aiswarya H <aiswarya12hari@gmail.com>
2026-06-10 12:17:16 +0530

    splash Screen

--- lib/VIEW/Splash Page/splash_screen.dart
+++ lib/VIEW/Splash Page/splash_screen.dart
@@ -1,32 +1,84 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:offixo/SERVICES/shared_preference_service.dart';
+import 'package:offixo/VIEW/Checkin%20page/checkinout.dart';
+import 'package:offixo/VIEW/Login%20page/login_screen.dart';
 import 'package:offixo/VIEW/Onboarding%20page/onboarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<SplashScreen> createState() =>
+      _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState
+    extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    _decideRoute();
+  }
 
-    Timer(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
+  Future<void> _decideRoute() async {
+    // Keep splash visible for 2 seconds
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+
+    final token =
+        await SharedPreferenceService
+            .getAccessToken();
+
+    if (!mounted) return;
+
+    /// TOKEN EXISTS
+    if (token != null &&
+        token.isNotEmpty) {
+      final isValid =
+          await SharedPreferenceService
+              .validateAccessToken();
+
+      if (!mounted) return;
+
+      if (isValid) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (_) =>
+                const CheckinScreen(),
+          ),
+          (route) => false,
+        );
+      } else {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(
+            builder: (_) =>
+                const LoginScreen(),
+          ),
+          (route) => false,
+        );
+      }
+    }
+
+    /// NO TOKEN
+    else {
+      Navigator.pushAndRemoveUntil(
         context,
         MaterialPageRoute(
-          builder: (_) => const OnboardingScreen(),
+          builder: (_) =>
+              const OnboardingScreen(),
         ),
+        (route) => false,
       );
-    });
+    }
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
@@ -40,7 +92,4 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
-
-
-
 
